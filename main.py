c870041cb432837a4474d93a58a282c6a7e3a90e
Timofei <t.a.maksimenko@ya.ru>
2022-08-14 22:50:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import json
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+
+from models import create_tables, Publisher, Shop, Book, Stock, Sale
+
+db_login = 'postgres'
+db_password = 'Postgres88'
+db_name = 'netology_homework'
+
+DSN = f'postgresql://{db_login}:{db_password}@localhost:5432/{db_name}'
+engine = sqlalchemy.create_engine(DSN)
+
+Session = sessionmaker(engine)
+session = Session()
+
+create_tables(engine)
+
+
+def import_data():
+    with open('fixtures/tests_data.json', 'r') as fd:
+        data = json.load(fd)
+
+        for record in data:
+            model = {
+                'publisher': Publisher,
+                'shop': Shop,
+                'book': Book,
+                'stock': Stock,
+                'sale': Sale,
+            }[record.get('model')]
+            session.add(model(id=record.get('pk'), **record.get('fields')))
+        session.commit()
+
+
+def publisher_search_name():
+    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
+    query_publisher_name = input('Введите имя (name) издателя: ')
+    query_result = query_join.filter(Publisher.name == query_publisher_name)
+    for result in query_result.all():
+        print(f'Издатель "{query_publisher_name}" реализуется в магазине "{result.name}" с идентификатором {result.id}')
+
+
+def publisher_search_id():
+    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
+    query_publisher_name = input('Введите идентификатор издателя: ')
+    query_result = query_join.filter(Publisher.id == query_publisher_name)
+    for result in query_result.all():
+        print(
+            f'Издатель c id: {query_publisher_name} реализуется в магазине "{result.name}" '
+            f'с идентификатором {result.id}')
+
+
+if __name__ == '__main__':
+    import_data()
+    publisher_search_name()
+    publisher_search_id()
---
+++ models.py
@@ -0,0 +1,71 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class Publisher(Base):
+    __tablename__ = 'publisher'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(40), nullable=False, unique=True)
+
+    def __str__(self):
+        return f'Publisher {self.id} : {self.name}'
+
+
+class Shop(Base):
+    __tablename__ = 'shop'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(40), nullable=False)
+
+    def __str__(self):
+        return f'Shop {self.id} : {self.name}'
+
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(40), nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
+    publisher = relationship(Publisher, backref='books')
+
+    def __str__(self):
+        return f'Book {self.id} : ({self.title}, {self.id_publisher})'
+
+
+class Stock(Base):
+    __tablename__ = 'stock'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    count = sq.Column(sq.Integer, nullable=False)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
+
+    book = relationship(Book, backref='stocks')
+    shop = relationship(Shop, backref='stocks')
+
+    def __str__(self):
+        return f'Stock {self.id} : ({self.count}, {self.id_book}, {self.id_shop})'
+
+
+class Sale(Base):
+    __tablename__ = 'sale'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Numeric, nullable=False)
+    date_sale = sq.Column(sq.DateTime, nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
+
+    stock = relationship(Stock, backref='sales')
+
+    def __str__(self):
+        return f'Sale {self.id} : ({self.price}, {self.date_sale}, {self.count}, {self.id_stock})'
+
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
